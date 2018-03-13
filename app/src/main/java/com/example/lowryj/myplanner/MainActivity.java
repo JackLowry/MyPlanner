23d6d9a9199aa838e2bc4411b71596ff26d7e16a
Jack Lowry <jackthechanFTW@gmail.com>
2018-03-13 17:25:05 -0400

    changes

--- app/src/main/java/com/example/lowryj/myplanner/MainActivity.java
+++ app/src/main/java/com/example/lowryj/myplanner/MainActivity.java
@@ -2,12 +2,27 @@ package com.example.lowryj.myplanner;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.CalendarView;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    private CalendarView cView;
+    private Toolbar tb;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        cView = findViewById(R.id.pView);
+        Toolbar tb = findViewById(R.id.my_toolbar);
+        setSupportActionBar(tb);
+    }
+
+    public void onClick(View v) {
+
     }
 }
---
+++ app/src/main/java/com/example/lowryj/myplanner/PlannerView.java
@@ -0,0 +1,22 @@
+package com.example.lowryj.myplanner;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.CalendarView;
+
+/**
+ * Created by jack on 2/5/18.
+ */
+
+public class PlannerView extends CalendarView {
+
+    public PlannerView(Context c) {
+        super(c);
+    }
+
+    public PlannerView(Context c, AttributeSet attr) {
+        super(c, attr);
+    }
+
+
+}
